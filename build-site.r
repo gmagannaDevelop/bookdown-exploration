da37ebda6314eb29ca60d4f2e060a51ee7fb7f10
Gustavo Magaña López <28574085+gmagannaDevelop@users.noreply.github.com>
2021-11-12 09:38:09 +0100

    :sparkles: added pdf and ebook
    .

---
+++ build-site.r
@@ -0,0 +1,8 @@
+
+library(bookdown)
+library(here)
+library(glue)
+
+bookdown::render_book("index.Rmd", "bookdown::gitbook")
+bookdown::render_book("index.Rmd", "bookdown::pdf_book")
+bookdown::render_book("index.Rmd", "bookdown::epub_book")
